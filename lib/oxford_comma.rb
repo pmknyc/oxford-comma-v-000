03f55cf22b75a46a4c84d6ae77daeb6208fe399c
Paula McKinley <pmknyc@optimum.net>
2018-10-05 16:06:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,16 +4,16 @@ reeses = ["PB", "chocolate"]
 blt = ["bacon"]
 
 def oxford_comma(array)
-    case array.length
-      when 0
-        puts "Array is empty"
-      when 1
-        array[0]
-      when 2
-        array.join(" and ")
+  case array.length
+    when 0
+      puts "Array is empty"
+    when 1
+      array[0]
+    when 2
+      array.join(" and ")
     else
       if array.length > 2
-      array[0...-1].join(", ") << ", and #{array[-1]}"
+        array[0...-1].join(", ") << ", and #{array[-1]}"
       end
-    end
+  end
 end
